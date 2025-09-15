3fffe600b8d1b816914f8421806c336b8fc5b0ea
Ageev-Danilo <danilaageev09@gmail.com>
2025-09-15 18:02:17 +0200

    Added weekday function

--- dz.js
+++ dz.js
@@ -4,3 +4,6 @@ function getCurrentMonth(){console.log(moment().format('MMMM'))}
 function getCurrentYear(){console.log(moment().format('YYYY'))} 
 function getDate(){console.log(moment().format('YYYY/DD/MM HH:mm:ss'))}
 getDate()
+
+function getCurrentWeekday(){console.log(moment().format('dddd'))}
+getCurrentWeekday()

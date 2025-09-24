b96965fc10704d906890e99ce86bd976a40ee9f4
Ageev-Danilo <danilaageev09@gmail.com>
2025-09-24 17:32:53 +0200

    New GET request on /posts

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-
-
+const fs = require('fs')
+const path = require('path')
 const express = require('express')
 const app = express()
 const HOST = 'localhost'
 const PORT = 8000
 app.listen(PORT, HOST, () => {console.log('Success! Server is running http://localhost:8000')})
+const postsPath = path.join(__dirname, "posts.json")
+const posts = JSON.parse(fs.readFileSync(postsPath, 'utf-8'))
+
+
 app.get('/timestamp', (req, res) => {
     console.log('New GET request')
     res.json(getDate())
+})
+app.get('/posts',(req, res) => {
+    console.log(posts)
+    res.json(posts)
 })
 
 const moment = require('moment')
